0707415bbdebffda6f410e553efd96de8ab9ba6a
matheus.luz <matheus.luz@corensc.gov.br>
2025-06-10 11:08:58 -0300

    🐛fix: Arrumado o schedule da aplicação.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -2,6 +2,7 @@
 
 use App\Http\Middleware\HandleAppearance;
 use App\Http\Middleware\HandleInertiaRequests;
+use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -26,6 +27,10 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias(
             ['role' => CheckRole::class]
         );
+    })
+    ->withSchedule(function (Schedule $schedule) {
+        $schedule->command('eventos:inativar-expirados')->dailyAt('00:00');
+        $schedule->command('ldap:sync-users')->everyTwoHours();
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
